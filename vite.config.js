f80bee78d52d0a91d29065888493ac3a9e756ad3
cepiseptiyana <cepifams3@gmail.com>
2025-01-20 15:32:22 +0700

    base:pangcuy

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/Pangcuy/",
+  base: "/pangcuy/",
   plugins: [vue()],
   resolve: {
     alias: {
