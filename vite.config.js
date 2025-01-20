ef9f0b24274d7c25919bea3838b6e7bc30f84e32
cepiseptiyana <cepifams3@gmail.com>
2025-01-20 15:13:19 +0700

    add dist

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vite.dev/config/
 export default defineConfig({
-  // base: "/Pangcuyy/",
+  base: "/Pangcuyy/",
   plugins: [vue()],
   resolve: {
     alias: {
